c0bcc93f06d7b8a7bff40abf366c207885f1547b
EC2 Default User <ec2-user@ip-172-31-13-225.ec2.internal>
2023-11-23 07:34:57 +0000

    Add Auth function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "viteprojectec59d3b7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,30 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <v-toolbar color="#242c3c" theme="dark" >
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <authenticator>
+    <template v-slot="{ user, signOut }"> <!-- 追加  -->
+      <v-toolbar color="#242c3c">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-    <v-toolbar-title>Amplify Vue Workshop</v-toolbar-title>
+        <v-toolbar-title>Amplify Vue Workshop</v-toolbar-title>
 
-    <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-    <v-btn  icon>
-      <v-icon>mdi-logout</v-icon>
-    </v-btn>
+        <v-btn @click="signOut" icon>
+          <v-icon>mdi-logout</v-icon>
+        </v-btn>
 
-    <template v-slot:extension>
-      <v-tabs align-tabs="title">
-        <v-tab to="/">HOME</v-tab>
-        <v-tab to="/chat">CHAT</v-tab>
-        <v-tab to="/ai">AI/ML</v-tab>
-        <v-tab to="/geo">GEO</v-tab>
-      </v-tabs>
-    </template>
-  </v-toolbar>
-  <RouterView />
+        <template v-slot:extension>
+          <v-tabs align-tabs="title">
+            <v-tab to="/">HOME</v-tab>
+            <v-tab to="/chat">CHAT</v-tab>
+            <v-tab to="/ai">AI/ML</v-tab>
+            <v-tab to="/geo">GEO</v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+      <RouterView :username="user.username" /> <!-- 追加  -->
+    </template> <!-- 追加  -->
+  </authenticator>
 </template>
+
